f5188364034998567b14924c3ebdf9b701159610
Fauzan Achmad <fauzan.blitz@gmail.com>
2019-05-20 12:21:11 +0700

    Initial Commit

---
+++ LavaLamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LavaLamp
+//
+//  Created by Fauzan Achmad on 20/05/19.
+//  Copyright © 2019 Fauzan Achmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
